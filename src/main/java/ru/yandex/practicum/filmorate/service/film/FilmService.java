3d5e451bb8a6f5e22e088f4499d15c3b7e3d3e75
kr8182 <kr81828@gmail.com>
2025-10-29 17:28:20 +0300

    11 спринт, Пробуем снова, так как добавили новые зависимости

--- src/main/java/ru/yandex/practicum/filmorate/service/film/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/film/FilmService.java
@@ -9,6 +9,7 @@ import ru.yandex.practicum.filmorate.messages.LogMessages;
 import ru.yandex.practicum.filmorate.messages.ValidationExceptionMessages;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.AbstractService;
+import ru.yandex.practicum.filmorate.service.user.UserService;
 import ru.yandex.practicum.filmorate.storage.Storage;
 
 import java.time.LocalDate;
@@ -21,10 +22,12 @@ import java.util.stream.Collectors;
 public class FilmService extends AbstractService<Film> {
     private static final LocalDate BOUNDARY_DATE = LocalDate.of(1895, 12, 28);
     private static final Comparator<Film> LIKES_COMPARATOR = (o1, o2) -> Integer.compare(o2.getLikes().size(), o1.getLikes().size());
+    private final UserService userService;
 
     @Autowired
-    public FilmService(Storage<Film> storage) {
+    public FilmService(Storage<Film> storage, UserService userService) {
         super.storage = storage;
+        this.userService = userService; // Инициализируем userService
     }
 
     @Override
@@ -36,8 +39,8 @@ public class FilmService extends AbstractService<Film> {
     }
 
     public Film addLike(int filmId, int userId) {
-        getById(filmId);
-        storage.getById(userId);
+        getById(filmId); // Проверяем существование фильма
+        userService.getById(userId);
         storage.getById(filmId).getLikes().add(userId);
         log.info(String.valueOf(LogMessages.LIKE_DONE), userId, filmId);
         return storage.getById(filmId);
@@ -45,6 +48,7 @@ public class FilmService extends AbstractService<Film> {
 
     public Film removeLike(int filmId, int userId) {
         getById(filmId);
+        userService.getById(userId);
         if (!storage.getById(filmId).getLikes().contains(userId))
             throw new NotFoundException(String.valueOf(ValidationExceptionMessages.WITHOUT_LIKE));
         storage.getById(filmId).getLikes().remove(userId);
